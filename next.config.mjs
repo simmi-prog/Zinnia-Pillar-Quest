ae1b05e065157491c847bfb149ad44d2bec1866f
simmi-prog <simmi.kumari@zinnia.com>
2026-03-17 11:31:43 +0530

    Setup GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/Zinnia-Pillar-Quest",
+  assetPrefix: "/Zinnia-Pillar-Quest/",
 };
 
 export default nextConfig;
